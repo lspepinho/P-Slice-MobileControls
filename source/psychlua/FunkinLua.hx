4d60095a95ffea3a38d697b542c8e631af680c9a
lspepinho <162769565+lspepinho@users.noreply.github.com>
2024-11-12 18:51:55 -0300

    fix

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -1571,10 +1571,7 @@ class FunkinLua {
 		CustomSubstate.implement(this);
 		ShaderFunctions.implement(this);
 		DeprecatedFunctions.implement(this);
-		#if TOUCH_CONTROLS_ALLOWED
 		MobileFunctions.implement(this);
-		MobileDeprecatedFunctions.implement(this);
-		#end
 		#if android AndroidFunctions.implement(this); #end
 
 		for (name => func in customFunctions)
